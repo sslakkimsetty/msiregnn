44fee18926afa465d1d003ebac2a031942db442b
Sai Srikanth Lakkimsetty <eolebgpc6@mozmail.com>
2024-06-19 15:47:02 -0400

    Export methods for utils, model, and training

--- src/msiregnn/__init__.py
+++ src/msiregnn/__init__.py
@@ -1,8 +1,15 @@
 """A neural network based framework for coregistering mass spec images with other modalities."""
 
 from .api import *  # noqa
-from .metrics import mi
+from . import metrics
 from .stn import (
     SpatialTransformerBspline,
     SpatialTransformerAffine
 )
+from .model import (
+    LocNet,
+    TransformationRegressor,
+    BsplineRegistration
+)
+from . import utils
+from . import training
