5fd7b60661cec437181b83934b58f86a01704283
Sai Srikanth Lakkimsetty <eolebgpc6@mozmail.com>
2025-08-08 03:26:25 -0400

    style: reorder imports in package __init__ per isort
    Reorders imports in src/msiregnn/__init__.py to align with import conventions and avoid import-order warnings.

--- src/msiregnn/__init__.py
+++ src/msiregnn/__init__.py
@@ -1,4 +1,4 @@
 """A neural network based framework for coregistering mass spec images with other modalities."""
 
-from . import metrics, reg, utils
 from .stn import SpatialTransformerAffine, SpatialTransformerBspline
+from . import metrics, reg, utils
